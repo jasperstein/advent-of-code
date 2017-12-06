1db87d5d65c1a95865d608da86f2219f117095a8
Jasper Stein <jstein@xebialabs.com>
2017-12-06 08:28:04 +0100

    Day 6, star 1

---
+++ src/main/scala/day6/Balancing.scala
@@ -0,0 +1,35 @@
+package day6
+
+import scala.collection.mutable
+
+object Balancing extends App {
+  def stringToInts(input: String): Array[Int] = input.split("\\s+").map(c => Integer.parseInt(c))
+
+  implicit class MemStepper(mem: Array[Int]) {
+    def step(): Unit = {
+      val max = mem.max
+      val maxidx = mem.indexOf(max)
+      mem(maxidx) = 0
+      for (i <- 1 to max) {
+        mem((maxidx + i) % mem.length) += 1
+      }
+    }
+  }
+
+  def countSteps(mem: Array[Int]): Unit = {
+    var seen = mutable.Set[Array[Int]]()
+    while (!seen.exists(_.sameElements(mem))) {
+      seen.add(Array[Int](mem: _*))
+      mem.step()
+    }
+    println(seen.size)
+  }
+
+  val example = "0 2 7 0"
+
+  countSteps(stringToInts(example))
+
+  val star1 = """10	3	15	10	5	15	5	15  9	2	5	8	5	2	3	6"""
+
+  countSteps(stringToInts(star1))
+}
