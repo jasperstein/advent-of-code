f2ae98829c4e928e1bb71acb6b98d49b6536627d
Jasper Stein <jstein@xebialabs.com>
2017-12-07 15:39:56 +0100

    Day 7, star 2

--- src/main/scala/day7/Hanoi.scala
+++ src/main/scala/day7/Hanoi.scala
@@ -1,10 +1,15 @@
 package day7
+import scala.collection.immutable
 
 object Hanoi extends App {
 
   var weights = Map[String, Int]() // we might just need these later...
 
   var parents = Map[String, String]()
+
+  var children = Map[String, List[String]]()
+
+  var subtreeWeights = Map[String, Int]()
 
   def parseWeight(spec: String): ((String, Int)) = {
     val split = spec.split("\\(").map(_.trim)
@@ -25,14 +30,43 @@ object Hanoi extends App {
     }
   }
 
-  def run(input: String): Unit = {
+  def root(input: String): String = {
     weights = Map()
     parents = Map()
+    children = Map()
+    subtreeWeights = Map()
     input.lines.foreach(parseLine)
-    println(weights.keySet diff parents.keySet)
+    (weights.keySet diff parents.keySet).head
   }
 
-  run(Input.example1)
-  run(Input.star1)
+  private val exampleRoot: String = root(Input.example1)
+  println(exampleRoot)
+  children = computeTree()
+  calcSubtreeWeights(exampleRoot)
+
+  private val star1root: String = root(Input.star1)
+  children = computeTree()
+  calcSubtreeWeights(star1root)
+
+  def computeTree(): Map[String, List[String]] = {
+    var tree = Map[String, List[String]](weights.keys.map(node => (node, List())).toSeq: _*)
+    for ((child, parent) <- parents) {
+      tree = tree + (parent -> (tree(parent) :+ child))
+    }
+    tree
+  }
+
+  def calcSubtreeWeights(node: String): Unit = {
+    val nodeChildren = children(node)
+    nodeChildren.foreach(calcSubtreeWeights)
+    val childWeights: immutable.Seq[Int] = nodeChildren.map(subtreeWeights)
+    if (!childWeights.forall(_ == childWeights.headOption.getOrElse(0))) {
+      nodeChildren.zip(childWeights).foreach(println)
+      nodeChildren.foreach(node => println(node + weights(node)))
+    }
+    subtreeWeights = subtreeWeights + (node -> (childWeights.sum + weights(node)))
+  }
+
+  // some manual mangling still needs to be done for star 2 ;-)
 
 }
