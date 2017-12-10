e834a6fe030e79c86a21b19c3902e969e4184628
Jasper Stein <jstein@xebialabs.com>
2017-12-10 11:22:36 +0100

    Day 9, star 2

--- src/main/scala/day9/Streams.scala
+++ src/main/scala/day9/Streams.scala
@@ -2,7 +2,7 @@ package day9
 
 object Streams extends App {
 
-  case class ParseState(currentScore: Int, depth: Int, garbage: Boolean, ignoreNext: Boolean)
+  case class ParseState(currentScore: Int, garbageCount: Int, depth: Int, garbage: Boolean, ignoreNext: Boolean)
 
   def transition(state: ParseState, ch: Char): ParseState = {
     if (state.ignoreNext) state.copy(ignoreNext = false)
@@ -10,7 +10,7 @@ object Streams extends App {
       ch match {
         case '>' => state.copy(garbage = false)
         case '!' => state.copy(ignoreNext = true)
-        case _ => state
+        case _ => state.copy(garbageCount = state.garbageCount + 1)
       }
     } else {
       ch match {
@@ -22,7 +22,7 @@ object Streams extends App {
     }
   }
 
-  val beginState = ParseState(0, 0, garbage = false, ignoreNext = false)
+  val beginState = ParseState(0, 0, 0, garbage = false, ignoreNext = false)
 
   def score(input: String): Int = input.foldLeft(beginState)(transition).currentScore
 
@@ -35,5 +35,15 @@ object Streams extends App {
   println(score(Input.example7))
   println(score(Input.example8))
   println(score(Input.star1))
+
+  def garbage(input: String): Int = input.foldLeft(beginState)(transition).garbageCount
+  println(garbage(Input.garbage1))
+  println(garbage(Input.garbage2))
+  println(garbage(Input.garbage3))
+  println(garbage(Input.garbage4))
+  println(garbage(Input.garbage5))
+  println(garbage(Input.garbage6))
+  println(garbage(Input.garbage7))
+  println(garbage(Input.star1))
 
 }
