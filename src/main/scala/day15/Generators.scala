94f2ecc2e6155eb2614af84135548c50719378d7
Jasper Stein <jstein@xebialabs.com>
2017-12-15 10:23:32 +0100

    Day 15, star 2

--- src/main/scala/day15/Generators.scala
+++ src/main/scala/day15/Generators.scala
@@ -6,26 +6,27 @@ object Generators extends App {
   var a = 722
   var b = 354
 
-  def matches(genA:Long, genB:Long, factorA: Int = 16807, factorB: Int = 48271, rounds: Int = 40000000): Int = {
+  // see previous commit for star 1
+  def matches(genA:Long, genB:Long, factorA: Int = 16807, factorB: Int = 48271, rounds: Int = 5000000): Int = {
     var a = genA
     var b = genB
     var found = 0
+    def step(gen: Long, factor: Int, mod: Int):Long = {
+      val result = (gen * factor) % 2147483647
+      if (result % mod == 0) result else step(result, factor, mod)
+    }
     for (i <- Range(0, rounds)) {
-      a = (a * factorA) % 2147483647
-      b = (b * factorB) % 2147483647
+      a = step(a, factorA, 4)
+      b = step(b, factorB, 8)
       val aStr = a.toBinaryString.takeRight(16)
       val bStr = b.toBinaryString.takeRight(16)
-//      println(s"$a\t$b")
-//      println(aStr)
-//      println(bStr)
-//      println()
       if (aStr.equals(bStr)) found += 1
     }
     found
   }
 
   // example
-  println(matches(65, 8921, rounds = 5))
+  println(matches(65, 8921, rounds = 1060))
   // for real
   println(matches(722, 354))
 }
