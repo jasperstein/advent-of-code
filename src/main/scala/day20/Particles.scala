acbb1300e397c6d4008c289a20a6eda6b2cc92d5
Jasper Stein <jstein@xebialabs.com>
2017-12-21 11:32:18 +0100

    Day 20, star 2

--- src/main/scala/day20/Particles.scala
+++ src/main/scala/day20/Particles.scala
@@ -16,4 +16,25 @@ object Particles extends App {
   println (parseInput(Input.star1).zipWithIndex.minBy(_._1.a.manhattan))
   println (parseInput(Input.star1).zipWithIndex.filter(_._1.a.manhattan == 2))
 
+  def step(p: Particle): Particle = {
+    val newV = Vect(p.v.x + p.a.x, p.v.y + p.a.y, p.v.z + p.a.z)
+    val newP = Vect(p.p.x + newV.x, p.p.y + newV.y, p.p.z + newV.z)
+    Particle(newP, newV, p.a)
+  }
+
+  def collide(ps: List[Particle]): List[Particle] = {
+    val coords = ps.map(_.p)
+    val collisions = coords.filter(p => coords.count(_ == p) > 1)
+    if (collisions.nonEmpty) println(collisions)
+    ps.filterNot(p => collisions.contains(p.p))
+  }
+
+  var particles = parseInput(Input.star1)
+  for (i <- Range(0, 20000)) { // 20000 should be enough for anybody
+    println(i)
+    particles = particles.map(step)
+    particles = collide(particles)
+  }
+  println(particles.size)
+
 }
